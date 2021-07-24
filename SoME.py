d3b2ddb1589f116199b6366fd03169147f195925
Thanniti Leelapattanaputichot <81139449+thanniti@users.noreply.github.com>
2021-07-24 23:19:46 +0700

    Update SoME.py

--- SoME.py
+++ SoME.py
@@ -3,48 +3,43 @@ import numpy as np
 import math
 import itertools
 ##Sierpinski build up##
-class Sierpinski1(Scene):
+class Sierpinski_Build_Up(Scene):
     def construct(self):
         #n = NumberPlane()
         #self.add(n)
-        self.color = itertools.cycle([RED,ORANGE,GREEN])
-        t1 = Triangle(fill_color=next(self.color),color=WHITE,fill_opacity=1,stroke_width=0)
+        WHITE_RED = "#FCDED7"
+        self.color = itertools.cycle([WHITE_RED,RED_A,RED_B,RED_C,RED_D,RED_E])
+        #colors = color_gradient(RED,WHITE)
+        t1 = Triangle(color=WHITE,fill_opacity=1,stroke_width=0)
+        t1.set_fill(WHITE_RED).scale(2.5)
         pos = [
         t1.get_vertices()[0],
         t1.get_vertices()[1],
         t1.get_vertices()[2],
         ]
         print(pos)
-        self.play(DrawBorderThenFill(t1))
+        self.play(Create(t1))
 
-        for _ in range(5-1):
+        for _ in range(7-1):
             mark = t1.copy()
+            mark.set_color(next(self.color))
             self.play(
                 t1.animate.scale(1/2,about_point=pos[0]),
                 rate_func=smooth,
-                run_time=2,
+                run_time=1.5,
                 )
             a = VGroup(t1)
             for i in range(2):
                 a.add(mark.copy().scale(1/2,about_point=pos[i+1])
-
                     )
             t1 = a
-            self.play(DrawBorderThenFill(a[1:]),run_time=3)
+            self.play(Create(a[1:]),run_time=3)
         
         self.wait()
 
 
 #########################################################################################################
 ## Testing Range Below ##
-from manim import*
-class Test(Scene):
-def getTri(self, color='#8B5F65'):
-    tri = Triangle()
-    tri.set_color(color)
-def construct(self):
-    tri = self.getTri()
-    self.play(Write(tri))
 
 
     
